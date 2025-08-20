a36cde768f140ebeffe2455165930d4da0cb407b
shparva <shparva@gap.com>
2025-08-20 12:24:05 +0530

    Commit file Check_palindrome.java

---
+++ Learn_the_basics/Know_basic_maths/Check_palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Check_palindrome {
+    // Problem Statement: Given an integer N, return true if it is a palindrome else return false.
+    // https://takeuforward.org/data-structure/check-if-a-number-is-palindrome-or-not/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int n = input;
+        int rev = 0;
+        while(n>0){
+            int r = n%10;
+            rev = r + rev*10;
+            n = n/10;
+        }
+
+        // System.out.println(rev);
+
+        if(rev == input){
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    
+}
+}
