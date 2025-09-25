35f234b660f2bec981e3594de163f803707d0225
shparva <shparva@gap.com>
2025-09-25 11:49:41 +0530

    new file:   Sorting/Quick_sort.java

---
+++ Sorting/Quick_sort.java
@@ -0,0 +1,15 @@
+package Sorting;
+
+public class Quick_sort {
+
+    public static void quickSort(int[] arr){
+        
+    }
+
+    public static void main(String[] args) {
+
+
+        
+    }
+    
+}
